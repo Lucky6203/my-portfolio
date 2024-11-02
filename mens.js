5dc1de578bf506b8c2443c9d047352ed0d57e529
Lucky kumar <152484721+Lucky6203@users.noreply.github.com>
2024-11-02 10:28:27 +0530

    Update mens.js

--- mens.js
+++ mens.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Web Developer"],
+    strings: [ "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
@@ -19,3 +19,24 @@ var typed = new Typed(".number", {
     backDelay: 1000,
     loop: true
 });
+
+// mail
+
+document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init("BkvrMHOLhfHLE79Jd");
+});
+
+function sendMail() {
+    var templateParams = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send('service_zptu0rs', 'template_2lunc02', templateParams)
+        .then(function(response) {
+            alert('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        })
+    }
